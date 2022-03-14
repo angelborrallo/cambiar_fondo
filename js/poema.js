5200defa4f8c2b5b8bac71142ef9dadea0297bdb
angelborrallo <93764800+angelborrallo@users.noreply.github.com>
2022-03-14 17:50:47 +0100

    Add files via upload

---
+++ js/poema.js
@@ -0,0 +1,9 @@
+
+function Version1(fondo1){
+    
+    var fonfo = document.body.style.backgroundImage = "url("../images/fondo.jpg")";
+    
+}
+function Version2(fondo2){
+    var fondo = document.body.style.backgroundImage = "url("../images/fondo2.jpg")";
+}
